e51942ae0c96cd9f679ce1d71a349476296cafd6
Abhishek Survase <abhishek.survase2000@gmail.com>
2026-08-17 00:10:51 +0530

    gh-pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.0.105"],
+  // allowedDevOrigins: ["192.168.0.105"],
+  output: "export",
+  assetPrefix: "/vrushketmore.github.io/",
+  basePath: "/vrushketmore.github.io",
+  // distDir: "docs",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
